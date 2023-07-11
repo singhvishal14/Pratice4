3b6f7b72782442498beb5876cd54ce6a233f1d04
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-07-11 11:54:05 +0530

    Fourth Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Username;
+            Console.WriteLine("Enter User Name");
+            Username = Console.ReadLine();
+            if(Username.Length>=6)
+            {
+                Console.WriteLine("Welcome: \t" + Username);
+            }
+            else
+            {
+                Console.WriteLine("Invalid User Name");
+            }
+            Console.ReadKey();
+        }
+    }
+}
